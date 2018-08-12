e5ea18c9460605ef055ec280521bf21811cffafe
Chris <cdietschrunfast@gmail.com>
2018-08-12 11:08:12 -0400

    finished chapter 34

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float valueRemaining = 10;
+    public float valueMaximum = 30;
+    public float valueConsumptionRate = 1f;
+
+    public Text timerText;
+
+    public bool gameInProgress = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameInProgress)
+        {
+            valueRemaining -= Time.deltaTime * valueConsumptionRate;
+            timerText.text = "Time Remaining: " + Mathf.Round(valueRemaining).ToString();
+
+            if (valueRemaining < 0)
+            {
+                gameInProgress = false;
+                EndGameWithFailure();
+            }
+        }
+    }
+
+    void EndGameWithFailure()
+    {
+
+    }
+}
